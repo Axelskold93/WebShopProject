b0ec59b715f877cad7ff2a489cb8edc071faba7f
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2024-04-04 15:10:15 +0200

    First Commit

---
+++ WebShop/Controllers/APIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Data;
+
+namespace WebShop.Controllers
+{
+    [Route("/api")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        private readonly AppDbContext database;
+
+        public APIController(AppDbContext database)
+        {
+            this.database = database;
+        }
+    }
+}
---
+++ WebShop/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebShop.Data;
+
+namespace WebShop.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext database;
+
+        public IndexModel(AppDbContext database)
+        {
+            this.database = database;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

9ecbde3630fb6ef22175b34e1690a414c246aeb3
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2024-04-08 13:39:05 +0200

    Added ShoppingCart
    Added Model for shoppingcart

--- WebShop/Models/Product.cs
+++ WebShop/Models/Product.cs
@@ -2,12 +2,12 @@
 {
 	public class Product
 	{
-		public int ProductId { get; set; }
+		public int ID { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public decimal Price { get; set; }
 
-		public string Category { get; set; }
+		public string? Category { get; set; }
 
 		public string? ImagePath { get; set; }
 		

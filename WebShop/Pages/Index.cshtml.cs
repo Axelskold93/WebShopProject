6688b3ccda13f98c406dd8b383fcd5b438ae87e2
s1ggelin <80677519+s1ggelin@users.noreply.github.com>
2024-04-10 15:30:02 +0200

    initiated order confirmation page

--- WebShop/Pages/Index.cshtml.cs
+++ WebShop/Pages/Index.cshtml.cs
@@ -1,17 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebShopProject.Data;
 using WebShopProject.Models;
 
 namespace WebShopProject.Pages
 {
-	public class IndexModel : PageModel
-	{
-		private readonly AppDbContext database;
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext database;
 
-		
 
-		
+
+
         private readonly AppDbContext _context;
 
         public IndexModel(AppDbContext context)

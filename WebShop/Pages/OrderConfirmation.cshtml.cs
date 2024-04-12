d699f1c91b8192b050caa3067d7597421d8dbc7d
Axelskold93 <142592414+Axelskold93@users.noreply.github.com>
2024-04-12 10:41:46 +0200

    Added OrderConfirmation backend Code

--- WebShop/Pages/OrderConfirmation.cshtml.cs
+++ WebShop/Pages/OrderConfirmation.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebShopProject.Data;
+using WebShopProject.Models;
 
 
 namespace WebShop.Pages
@@ -7,14 +8,26 @@ namespace WebShop.Pages
     public class OrderConfirmationModel : PageModel
     {
         private readonly AppDbContext _context;
+        private readonly AccessControl _accessControl;
 
-        public OrderConfirmationModel(AppDbContext context)
+        public OrderConfirmationModel(AppDbContext context, AccessControl accessControl)
         {
             _context = context;
+            Cart = new List<Product>();
+            _accessControl = accessControl;
         }
+
+        public List<Product>? Products { get; set; }
+        public List<Product> Cart;
+        public decimal TotalPrice { get; set; }
 
         public void OnGet()
         {
+            Products = _context.Products.ToList();
+
+
+            Cart = _context.Products.Where(p => p.Account != null && p.Account.ID == _accessControl.LoggedInAccountID).ToList();
+            TotalPrice = (decimal)Cart.Sum(p => p.Price);
         }
     }
 }
